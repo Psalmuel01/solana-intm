845d38a277a826ae6bfe6af5557314708bb09718
gyan0890 <gyanlakshmi@gmail.com>
2022-08-26 01:07:10 +0530

    Committing the Rust concept files

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+mod slice;
+mod structs;
+mod matching;
+
+fn main() {
+        //slice::slice_example();
+        //structs::struct_examples();
+        matching::matching_examples();
+}
+    
+    
+
+
+
+
---
+++ target/debug/deps/rust_part2-a05eec7ca092e0f1.d
@@ -0,0 +1,8 @@
+/Users/gyan/Documents/ProjectGyan/Metacrafters/Module3/rust_part2/target/debug/deps/rust_part2-a05eec7ca092e0f1: src/main.rs src/slice.rs src/structs.rs src/matching.rs
+
+/Users/gyan/Documents/ProjectGyan/Metacrafters/Module3/rust_part2/target/debug/deps/rust_part2-a05eec7ca092e0f1.d: src/main.rs src/slice.rs src/structs.rs src/matching.rs
+
+src/main.rs:
+src/slice.rs:
+src/structs.rs:
+src/matching.rs:
