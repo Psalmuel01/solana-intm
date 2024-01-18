71e7a1f42203901880226954e260c16990b2302e
Samuel Dahunsi <dahunsisamuel1st@gmail.com>
2024-01-18 07:42:58 +0000

    update calculator

--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,13 @@ use solana_program::{
     program_error::ProgramError,
     pubkey::Pubkey,
 };
+use std::convert::TryInto;
 
 /// Define the type of state stored in accounts
 #[derive(BorshSerialize, BorshDeserialize, Debug)]
-pub struct GreetingAccount {
-    /// number of greetings
-    pub counter: u32,
+pub struct CalculatorAccount {
+    /// The result of the operation
+    pub result: f64,
 }
 
 // Declare and export the program's entrypoint
@@ -20,47 +21,104 @@ entrypoint!(process_instruction);
 
 // Program entrypoint's implementation
 pub fn process_instruction(
-    program_id: &Pubkey, // Public key of the account the hello world program was loaded into
-    accounts: &[AccountInfo], // The account to say hello to
-    _instruction_data: &[u8], // Ignored, all helloworld instructions are hellos
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
 ) -> ProgramResult {
-    msg!("Hello World Rust program entrypoint");
+    msg!("Calculator Rust program entrypoint");
 
-    // Iterating accounts is safer than indexing
     let accounts_iter = &mut accounts.iter();
 
-    // Get the account to say hello to
     let account = next_account_info(accounts_iter)?;
 
-    // The account must be owned by the program in order to modify its data
+    // Create the program account if it doesn't exist
     if account.owner != program_id {
-        msg!("Greeted account does not have the correct program id");
-        return Err(ProgramError::IncorrectProgramId);
+        msg!("Creating calculator account");
+        let data = &mut account.data.borrow_mut();
+        // let mut calculator_account = CalculatorAccount { result: 0.0 };
+        let calculator_account = CalculatorAccount { result: 0.0 };
+        calculator_account
+            .serialize(&mut &mut data[..])
+            .map_err(|_| ProgramError::InvalidAccountData)?;
+        msg!("Calculator account created");
     }
 
-    // Increment and store the number of times the account has been greeted
-    let mut greeting_account = GreetingAccount::try_from_slice(&account.data.borrow())?;
-    greeting_account.counter += 1;
-    greeting_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
+    // let mut calculator_account = CalculatorAccount::try_from_slice(&account.data.borrow())?;
 
-    msg!("Greeted {} time(s)!", greeting_account.counter);
+    let mut calculator_account = {
+        let data = account.data.borrow();
+        CalculatorAccount::try_from_slice(&data[..])?
+    };
+
+    // Decode the instruction data to determine the operation
+    if instruction_data.len() < 1 {
+        msg!("Invalid instruction data");
+        return Err(ProgramError::InvalidInstructionData);
+    }
+
+    let operation = instruction_data[0];
+
+    // Handle the operation based on user input
+    match operation {
+        0 => {
+            // Addition operation
+            msg!("Performing addition");
+            if instruction_data.len() < 17 {
+                msg!("Invalid instruction data");
+                return Err(ProgramError::InvalidInstructionData);
+            }
+            let num1_bytes = &instruction_data[1..9];
+            let num2_bytes = &instruction_data[9..17];
+            let num1 = f64::from_le_bytes(num1_bytes.try_into().unwrap());
+            let num2 = f64::from_le_bytes(num2_bytes.try_into().unwrap());
+            calculate_sum(&mut calculator_account, num1, num2);
+        }
+        1 => {
+            // Subtraction operation
+            msg!("Performing subtraction");
+            if instruction_data.len() < 17 {
+                msg!("Invalid instruction data");
+                return Err(ProgramError::InvalidInstructionData);
+            }
+            let num1_bytes = &instruction_data[1..9];
+            let num2_bytes = &instruction_data[9..17];
+            let num1 = f64::from_le_bytes(num1_bytes.try_into().unwrap());
+            let num2 = f64::from_le_bytes(num2_bytes.try_into().unwrap());
+            calculate_difference(&mut calculator_account, num1, num2);
+        }
+        _ => {
+            msg!("Invalid operation");
+            return Err(ProgramError::InvalidInstructionData);
+        }
+    }
+
+    calculator_account.serialize(&mut &mut account.data.borrow_mut()[..])?;
 
     Ok(())
 }
 
-// Sanity tests
+// Sum function
+pub fn calculate_sum(account: &mut CalculatorAccount, num1: f64, num2: f64) {
+    account.result = num1 + num2;
+}
+
+// Difference function
+pub fn calculate_difference(account: &mut CalculatorAccount, num1: f64, num2: f64) {
+    account.result = num1 - num2;
+}
+
 #[cfg(test)]
-mod test {
+mod tests {
     use super::*;
     use solana_program::clock::Epoch;
     use std::mem;
 
     #[test]
-    fn test_sanity() {
+    fn test_calculator_contract() {
         let program_id = Pubkey::default();
         let key = Pubkey::default();
         let mut lamports = 0;
-        let mut data = vec![0; mem::size_of::<u32>()];
+        let mut data = vec![0; mem::size_of::<CalculatorAccount>()];
         let owner = Pubkey::default();
         let account = AccountInfo::new(
             &key,
@@ -72,29 +130,30 @@ mod test {
             false,
             Epoch::default(),
         );
-        let instruction_data: Vec<u8> = Vec::new();
+        let mut instruction_data = vec![0; 17];
 
-        let accounts = vec![account];
+        // Test addition operation
+        instruction_data[0] = 0;
+        let num1: f64 = 3.5; // Specify f64 type
+        let num2: f64 = 2.0; // Specify f64 type
+        instruction_data[1..9].copy_from_slice(&num1.to_le_bytes());
+        instruction_data[9..17].copy_from_slice(&num2.to_le_bytes());
 
-        assert_eq!(
-            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .counter,
-            0
-        );
+        let accounts = vec![account.clone()];
+
         process_instruction(&program_id, &accounts, &instruction_data).unwrap();
-        assert_eq!(
-            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .counter,
-            1
-        );
+        let result_account = CalculatorAccount::try_from_slice(&accounts[0].data.borrow()).unwrap();
+        assert_eq!(result_account.result, 5.5);
+
+        // Test subtraction operation
+        instruction_data[0] = 1;
+        let num1: f64 = 5.7; // Specify f64 type
+        let num2: f64 = 2.5; // Specify f64 type
+        instruction_data[1..9].copy_from_slice(&num1.to_le_bytes());
+        instruction_data[9..17].copy_from_slice(&num2.to_le_bytes());
+
         process_instruction(&program_id, &accounts, &instruction_data).unwrap();
-        assert_eq!(
-            GreetingAccount::try_from_slice(&accounts[0].data.borrow())
-                .unwrap()
-                .counter,
-            2
-        );
+        let result_account = CalculatorAccount::try_from_slice(&accounts[0].data.borrow()).unwrap();
+        assert_eq!(result_account.result, 3.2);
     }
 }
